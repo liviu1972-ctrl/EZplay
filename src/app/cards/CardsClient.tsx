3ee37ca043e9bfef61d83b0658b05415c6d9886a
cartobello <cartobello@gmail.com>
2026-05-26 20:07:10 +0300

    style(cards): remove hover translate-y and scale animation completely to keep images crisp

--- src/app/cards/CardsClient.tsx
+++ src/app/cards/CardsClient.tsx
@@ -300,7 +300,7 @@ export function CardsClient({ initialCards, cardTypes, assetTypes, lang, dict }:
             return (
               <div
                 key={card.id}
-                className="group relative cursor-pointer transition-all duration-300 hover:-translate-y-1 hover:scale-[1.01]"
+                className="group relative cursor-pointer"
                 style={{ perspective: "1000px" }}
                 onClick={(e) => toggleFlip(card.id, e)}
               >
